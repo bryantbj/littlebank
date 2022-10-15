0bc39a6c505a9f38e47d5f9c8261a3219dc80d5d
BJ Bryant <bryantb76@gmail.com>
2022-10-15 13:30:02 -0500

    Can add transactions

--- lib/little_bank_web/live/bank_account_live/form_component.ex
+++ lib/little_bank_web/live/bank_account_live/form_component.ex
@@ -26,28 +26,22 @@ defmodule LittleBankWeb.BankAccountLive.FormComponent do
   end
 
   def handle_event("save", %{"transaction" => trx_params}, socket) do
+    types = %{amount: :float, vendor: :string, note: :string, date: :date, credit: :boolean, bank_account_id: :integer}
+
+    trx_params = {%{}, types}
+    |> Ecto.Changeset.cast(trx_params, Map.keys(types))
+    |> Map.get(:changes)
+    |> Map.update!(:amount, & trunc(&1 * 100))
+
     save_transaction(socket, socket.assigns.action, trx_params)
   end
-
-  # defp save_transaction(socket, :edit, post_params) do
-  #   case Examples.update_post(socket.assigns.post, post_params) do
-  #     {:ok, _post} ->
-  #       {:noreply,
-  #        socket
-  #        |> put_flash(:info, "Post updated successfully")
-  #        |> push_redirect(to: socket.assigns.return_to)}
-
-  #     {:error, %Ecto.Changeset{} = changeset} ->
-  #       {:noreply, assign(socket, :changeset, changeset)}
-  #   end
-  # end
 
   defp save_transaction(socket, :new, trx_params) do
     case BankAccounts.bank_account_transaction(socket.assigns.bank_account, trx_params) do
       {:ok, _trx} ->
         {:noreply,
          socket
-         |> put_flash(:info, "Post created successfully")
+         |> put_flash(:info, "Transaction added successfully")
          |> push_redirect(to: socket.assigns.return_to)}
 
       {:error, %Ecto.Changeset{} = changeset} ->
